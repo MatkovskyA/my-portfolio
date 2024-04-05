fef68ca9d9434b3d634e02870a665f974d6f8d79
MatkovskyA <a.matkovsky14@gmail.com>
2024-04-05 10:08:49 +0300

    Initail project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css";
+
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+
+import vk from "./img/icons/vk.svg"
+import instagram from "./img/icons/instagram.svg"
+import linkedIn from "./img/icons/linkedIn.svg"
+import tg from "./img/icons/tg.svg"
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar />
+      <Header />
+
+      <main className="section">
+        <div className="container">
+          <ul className="content-list">
+            <li className="content-list__item">
+              <h2 className="title-2">Frontend</h2>
+              <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
+            </li>
+            <li className="content-list__item">
+              <h2 className="title-2">Backend</h2>
+              <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
+            </li>
+          </ul>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <div className="container">
+          <div className="footer__wrapper">
+            <ul className="social">
+              <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
+              <li className="social__item"><a href="#!"><img src={instagram} alt="Link" /></a></li>
+              <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link" /></a></li>
+              <li className="social__item"><a href="#!"><img src={tg} alt="Link" /></a></li>
+            </ul>
+            <div className="copyright">
+              <p>© 2022 frontend-dev.com</p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
